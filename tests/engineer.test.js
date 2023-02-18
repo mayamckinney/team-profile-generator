88f8115c0775ac17a77bafe2c11c3e29e062edcd
Maya McKinney <mayamckinney0306@gmail.com>
2023-02-18 15:12:22 -0700

    swapping inputs to bugfix

--- tests/engineer.test.js
+++ tests/engineer.test.js
@@ -1,14 +1,14 @@
 const Engineer = require('../lib/engineer');
 
 test("Create a new engineer", () => {
-    const employeeTest = new Engineer(110011, 'John Doe', 'johndoe@employee.com');
+    const employeeTest = new Engineer('John Doe', 110011, 'johndoe@employee.com');
     expect(employeeTest.name).toBe('John Doe');
     expect(employeeTest.id).toEqual(expect.any(Number));
     expect(employeeTest.email).toEqual(expect.any(String));
 });
 
 test("All fields are filled out", () => {
-    const employeeTest = new Engineer(110011, 'John Doe', 'johndoe@employee.com');
+    const employeeTest = new Engineer('John Doe', 110011, 'johndoe@employee.com');
     expect(employeeTest.getName()).toBe(employeeTest.name);
     expect(employeeTest.getId()).toBe(employeeTest.id);
     expect(employeeTest.getEmail()).toBe(employeeTest.email);
